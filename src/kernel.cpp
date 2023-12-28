21680cb3cf13fd7527f5973295c9095a84a59b51
ergo720 <45463469+ergo720@users.noreply.github.com>
2023-12-29 00:56:09 +0100

    Return a clock increment to the kernel queries so that it can update correctly the system time read from the CMOS

--- src/kernel.cpp
+++ src/kernel.cpp
@@ -11,13 +11,27 @@
 #include <assert.h>
 
 
-static uint64_t io_id;
+static uint64_t io_id, lost_clock_increment, last_us, curr_us;
+
+static uint64_t
+calculate_kernel_clock_increment()
+{
+	// NOTE: a clock interrupt is generated at every ms, so ideally the increment should always be 10000 -> 10000 * 100ns units = 1ms
+	curr_us = get_now();
+	uint64_t elapsed_us = curr_us - last_us;
+	last_us = curr_us;
+	uint64_t elapsed_clock_increment = elapsed_us * 10;
+	lost_clock_increment += elapsed_clock_increment;
+	uint64_t actual_clock_increment = ((lost_clock_increment / 10000) * 10000); // floor to the nearest multiple of clock increment
+	lost_clock_increment -= actual_clock_increment;
+
+	return actual_clock_increment;
+}
 
 uint32_t
 nboxkrnl_read_handler(addr_t addr, void *opaque)
 {
-	static uint64_t curr_time;
-	static uint64_t acpi_time;
+	static uint64_t acpi_time, curr_clock_increment;
 
 	switch (addr)
 	{
@@ -25,16 +39,16 @@ nboxkrnl_read_handler(addr_t addr, void *opaque)
 		// For now, we always want an xbox system. 0: xbox, 1: chihiro, 2: devkit
 		return 0;
 
-	case BOOT_TIME_LOW:
+	case CLOCK_INCREMENT_LOW:
 		// These three are read in succession from the clock isr with interrupts disabled, so we can read the boot time only once instead of three times
-		curr_time = get_now();
-		return static_cast<uint32_t>(curr_time / 100);
+		curr_clock_increment = calculate_kernel_clock_increment();
+		return static_cast<uint32_t>(curr_clock_increment);
 
-	case BOOT_TIME_HIGH:
-		return (curr_time / 100) >> 32;
+	case CLOCK_INCREMENT_HIGH:
+		return curr_clock_increment >> 32;
 
 	case BOOT_TIME_MS:
-		return static_cast<uint32_t>(curr_time / 1000);
+		return static_cast<uint32_t>(curr_us / 1000);
 
 	case IO_CHECK_ENQUEUE:
 		return pending_packets;
