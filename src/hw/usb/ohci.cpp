b7811fbf0e6a9c92a4597c457661c3c563e0ca17
ergo720 <45463469+ergo720@users.noreply.github.com>
2025-08-04 03:40:03 +0200

    Handled some more registers of OHCI

--- src/hw/usb/ohci.cpp
+++ src/hw/usb/ohci.cpp
@@ -3,12 +3,37 @@
 // SPDX-FileCopyrightText: 2025 ergo720
 
 #include "machine.hpp"
+#include <algorithm>
 
 #define MODULE_NAME usb0
 
 #define usb0_log_read() m_machine->log_read<log_module::MODULE_NAME, false>(m_regs_info, addr, value);
 #define usb0_log_write() m_machine->log_write<log_module::MODULE_NAME, false>(m_regs_info, addr, value);
 
+#define CTRL_HCFS (3 << 6) // HostControllerFunctionalState
+#define CMD_ST_HCR (1 << 0) // SchedulingOverrunCount
+#define CMD_ST_SOC (3 << 16) // SchedulingOverrunCount
+#define CMD_ST_RO_MASK CMD_ST_SOC
+#define RHDA_NDP 0xFF // NumberDownstreamPorts
+#define RHDA_PSM (1 << 8) // PowerSwitchingMode
+#define RHDA_NPS (1 << 9) // NoPowerSwitching
+#define RHDA_DT (1 << 10) // DeviceType
+#define RHDA_NOCP (1 << 12) // NoOverCurrentProtection
+#define RHDA_RO_MASK (RHDA_DT | RHDA_NDP)
+#define RHDB_PPCM(i) (1 << (17 + i)) // PortPowerControlMask
+#define RH_ST_LPS (1 << 0) // LocalPowerStatus, ClearGlobalPower
+#define RH_ST_OCI (1 << 1) // OverCurrentIndicator
+#define RH_ST_DRWE (1 << 15) // DeviceRemoteWakeupEnable
+#define RH_ST_LPSC (1 << 16) // LocalPowerStatusChange, SetGlobalPower
+#define RH_ST_CRWE (1 << 31) // ClearRemoteWakeupEnable
+#define RH_PORT_ST_PPS (1 << 8) // PortPowerStatus
+
+
+template<typename T>
+void usb0::update_port_status(T &&f)
+{
+	std::for_each(std::begin(m_port), std::end(m_port), f);
+}
 
 template<bool log>
 void usb0::write(uint32_t addr, const uint32_t value)
@@ -19,8 +44,58 @@ void usb0::write(uint32_t addr, const uint32_t value)
 
 	switch (addr)
 	{
-	case HC_REVISION:
+	case REVISION:
 		// read-only
+		break;
+
+	case CMD_ST:
+		REG_USB0(CMD_ST) |= (value & ~CMD_ST_RO_MASK);
+		if (value & CMD_ST_HCR) {
+			sw_reset();
+		}
+		break;
+
+	case RH_DESCRIPTOR_A:
+		REG_USB0(RH_DESCRIPTOR_A) = (value & ~RHDA_RO_MASK) | (REG_USB0(RH_DESCRIPTOR_A) & RHDA_RO_MASK);
+		break;
+
+	case RH_ST:
+		if (value & RH_ST_LPS) { // ClearGlobalPower
+			if ((REG_USB0(RH_DESCRIPTOR_A) & (RHDA_NPS | RHDA_PSM)) == 0) {
+				// global power mode: turn off power to all ports
+				update_port_status([](usb_port &p){ p.rh_port_status &= ~RH_PORT_ST_PPS; });
+			}
+			else if ((REG_USB0(RH_DESCRIPTOR_A) & (RHDA_NPS | RHDA_PSM)) == RHDA_PSM) {
+				// per-port power mode: turn off power for ports that have RHDB_PPCM cleared
+				update_port_status([this](usb_port &p)
+					{
+						if ((REG_USB0(RH_DESCRIPTOR_B) & RHDB_PPCM(p.idx)) == 0) {
+							p.rh_port_status &= ~RH_PORT_ST_PPS;
+						}
+					});
+			}
+		}
+		if (value & RH_ST_LPSC) { // SetGlobalPower
+			if ((REG_USB0(RH_DESCRIPTOR_A) & (RHDA_NPS | RHDA_PSM)) == 0) {
+				// global power mode: turn on power to all ports
+				update_port_status([](usb_port &p){ p.rh_port_status |= RH_PORT_ST_PPS; });
+			}
+			else if ((REG_USB0(RH_DESCRIPTOR_A) & (RHDA_NPS | RHDA_PSM)) == RHDA_PSM) {
+				// per-port power mode: turn on power for ports that have RHDB_PPCM cleared
+				update_port_status([this](usb_port &p)
+					{
+						if ((REG_USB0(RH_DESCRIPTOR_B) & RHDB_PPCM(p.idx)) == 0) {
+							p.rh_port_status |= RH_PORT_ST_PPS;
+						}
+					});
+			}
+		}
+		if (value & RH_ST_DRWE) { // SetRemoteWakeupEnable
+			REG_USB0(RH_ST) |= RH_ST_DRWE;
+		}
+		if (value & RH_ST_CRWE) { // ClearRemoteWakeupEnable
+			REG_USB0(RH_ST) &= ~RH_ST_DRWE;
+		}
 		break;
 
 	default:
@@ -58,10 +133,28 @@ usb0::update_io(bool is_update)
 }
 
 void
-usb0::reset()
+usb0::sw_reset()
+{
+	std::fill_n(std::begin(m_regs), (RH_DESCRIPTOR_A - USB0_BASE) / 4, 0);
+	REG_USB0(REVISION) = 0x10;
+
+	REG_USB0(CTRL) |= ((std::to_underlying(usb_state::suspend) << 6) & CTRL_HCFS);
+
+	logger_en(debug, "Suspend mode");
+}
+
+void
+usb0::hw_reset()
 {
 	std::fill(std::begin(m_regs), std::end(m_regs), 0);
-	REG_USB0(HC_REVISION) = 0x10;
+	REG_USB0(REVISION) = 0x10;
+	REG_USB0(RH_DESCRIPTOR_A) = RHDA_NPS | RHDA_NOCP | 4; // four ports for HC
+}
+
+void
+usb0::reset()
+{
+	hw_reset();
 }
 
 bool
--- src/hw/usb/ohci.hpp
+++ src/hw/usb/ohci.hpp
@@ -13,10 +13,21 @@
 #define REGS_USB0_idx(x) ((x - USB0_BASE) >> 2)
 #define REG_USB0(r) (m_regs[REGS_USB0_idx(r)])
 
-#define HC_REVISION (USB0_BASE + 0x00)
+#define REVISION (USB0_BASE + 0x00)
+#define CTRL (USB0_BASE + 0x04)
+#define CMD_ST (USB0_BASE + 0x08)
+#define RH_DESCRIPTOR_A (USB0_BASE + 0x48)
+#define RH_DESCRIPTOR_B (USB0_BASE + 0x4C)
+#define RH_ST (USB0_BASE + 0x50)
+#define RH_PORT_ST(i) (USB0_BASE + 0x54 + i * 4)
 
 
 class machine;
+
+struct usb_port {
+	uint32_t rh_port_status;
+	unsigned idx;
+};
 
 class usb0 {
 public:
@@ -30,12 +41,32 @@ public:
 	void write(uint32_t addr, const uint32_t value);
 
 private:
+	enum class usb_state : uint32_t {
+		reset = 0,
+		resume = 1,
+		operational = 2,
+		suspend = 3
+	};
 	bool update_io(bool is_update);
+	template<typename T>
+	void update_port_status(T &&f);
+	void hw_reset();
+	void sw_reset();
 
 	machine *const m_machine;
 	// registers
+	usb_port m_port[4];
 	uint32_t m_regs[USB0_SIZE / 4];
 	const std::unordered_map<uint32_t, const std::string> m_regs_info = {
-		{ HC_REVISION, "HcRevision" }
+		{ REVISION, "HcRevision" },
+		{ CTRL, "HcControl" },
+		{ CMD_ST, "HcCommandStatus" },
+		{ RH_DESCRIPTOR_A, "HcRhDescriptorA"},
+		{ RH_DESCRIPTOR_B, "HcRhDescriptorB"},
+		{ RH_ST, "HcRhStatus"},
+		{ RH_PORT_ST(0), "HcRhPortStatus0" },
+		{ RH_PORT_ST(1), "HcRhPortStatus1" },
+		{ RH_PORT_ST(2), "HcRhPortStatus2" },
+		{ RH_PORT_ST(3), "HcRhPortStatus3" },
 	};
 };
